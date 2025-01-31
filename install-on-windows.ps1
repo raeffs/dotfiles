a06db47f5d3302adbd4f878f9029f954500053c9
Raphael Fleischlin <git@raeffs.dev>
2025-01-31 16:10:49 +0100

    git: add config

--- install-on-windows.ps1
+++ install-on-windows.ps1
@@ -4,6 +4,8 @@ Param(
 
 $DotfilesDir = $PSScriptRoot
 
+New-Item -Path "$TargetDir\.gitconfig" -Type SymbolicLink -Value "$DotfilesDir\git\.gitconfig" -Force
+
 New-Item -Path "$TargetDir\.config\oh-my-posh\oh-my-posh.config.yml" -Type SymbolicLink -Value "$DotfilesDir\oh-my-posh\.config\oh-my-posh\oh-my-posh.config.yml" -Force
 New-Item -Path "$TargetDir\.config\starship.toml" -Type SymbolicLink -Value "$DotfilesDir\starship\.config\starship.toml" -Force
 New-Item -Path "$TargetDir\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Type SymbolicLink -Value "$DotfilesDir\powershell\.config\powershell\Microsoft.PowerShell_profile.Windows.ps1" -Force
