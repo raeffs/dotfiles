e964b7e3eef8830f2cc5389a1a2c3a7a5f96bdcb
Raphael Fleischlin <git@raeffs.dev>
2025-11-01 10:36:20 +0100

    use openssh for git on windows

--- install-on-windows.ps1
+++ install-on-windows.ps1
@@ -5,6 +5,7 @@ Param(
 $DotfilesDir = $PSScriptRoot
 
 New-Item -Path "$TargetDir\.gitconfig" -Type SymbolicLink -Value "$DotfilesDir\git\.gitconfig" -Force
+New-Item -Path "$TargetDir\windows.gitconfig" -Type SymbolicLink -Value "$DotfilesDir\git\windows\.gitconfig" -Force
 
 New-Item -Path "$TargetDir\.config\oh-my-posh\oh-my-posh.config.yml" -Type SymbolicLink -Value "$DotfilesDir\oh-my-posh\.config\oh-my-posh\oh-my-posh.config.yml" -Force
 New-Item -Path "$TargetDir\.config\starship.toml" -Type SymbolicLink -Value "$DotfilesDir\starship\.config\starship.toml" -Force
